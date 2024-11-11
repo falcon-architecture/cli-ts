514b976bf0bfc22ae5662e6d2ba44f3efa8af97b
Natarajan Ganapathi <natarajan_ganapathi@epam.com>
2024-11-11 20:19:06 +0530

    Refactor logger initialization to index.ts for improved organization and maintainability

--- src/cli.ts
+++ src/cli.ts
@@ -1,29 +1,6 @@
 #!/usr/bin/env node
 
 import { CommandBuilder } from './commandBuilder';
-import { LoggerBuilder } from './loggerBuilder';
-import winston from 'winston';
-
-global.loggerBuilder = LoggerBuilder.new()
-    .addTransport(
-        new winston.transports.Console({
-            format: LoggerBuilder.consoleFormat()
-        }),
-        new winston.transports.File({
-            filename: 'logs/cli.log',
-            level: 'info',
-            format: LoggerBuilder.fileFormat()
-        })
-    )
-    .setLevel('debug')
-    .setColors({
-        fatal: 'red',
-        error: 'red',
-        warn: 'yellow',
-        info: 'green',
-        debug: 'blue',
-        trace: 'magenta'
-    });
 
 CommandBuilder.new()
     .setName("do")
